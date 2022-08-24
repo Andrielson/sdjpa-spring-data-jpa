bcdf27bb6f1a34d74119708e0f4ab414130663fa
Andrielson Silva <andrielson@gmail.com>
2022-08-24 07:13:55 -0300

    First commit

---
+++ src/main/java/com/github/andrielson/sdjpaspringdatajpa/SdjpaSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.github.andrielson.sdjpaspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/andrielson/sdjpaspringdatajpa/SdjpaSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andrielson.sdjpaspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
